56bacc17e668d4454e8f29a2f6ded45c543e8979
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-07-14 11:05:57 -0300

    feat: Adiciona a classe ClienteDTO
     Closes #27

---
+++ src/main/java/com/seuprojeto/chatbot/dto/ClienteDTO.java
@@ -0,0 +1,17 @@
+package com.seuprojeto.chatbot.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ClienteDTO {
+    //@Id
+    //private final UUID id;
+    private String nome;
+    private String horarioMarcado;
+
+}
