e0460860bef029624e1e63ed5f6d2b4dd61570dd
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-09-07 13:07:54 -0300

    fix: adiciona os campos dia, horario e disponibilidade no HorarioDTO

--- src/main/java/com/seuprojeto/chatbot/dto/HorarioDTO.java
+++ src/main/java/com/seuprojeto/chatbot/dto/HorarioDTO.java
@@ -1,27 +1,8 @@
 package com.seuprojeto.chatbot.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class HorarioDTO {
-
-    @Id
     private Long id;
-
     private String dia;
-
     private String horario;
-
     private boolean disponivel = true;
-
 }
