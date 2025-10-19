8e20ce41ce927f152ad483e4b724956e3ca77f05
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-10-19 12:29:00 -0300

    feat: habilita integração com banco de dados e endpoints REST

--- src/main/java/com/seuprojeto/chatbot/ChatbotApplication.java
+++ src/main/java/com/seuprojeto/chatbot/ChatbotApplication.java
@@ -1,19 +1,11 @@
 package com.seuprojeto.chatbot;
 
-import com.seuprojeto.chatbot.controller.ChatbotController;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 
-//Para não precisar de um banco de dados
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ChatbotApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(ChatbotApplication.class, args);
-
-        ChatbotController chatbot = context.getBean(ChatbotController.class);
-        chatbot.iniciarAtendimento();
+        SpringApplication.run(ChatbotApplication.class, args);
     }
 }
